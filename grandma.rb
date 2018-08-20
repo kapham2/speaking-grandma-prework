864b891b8da8069763f856c7c4ae01fc09b3c641
Kim Pham <pham.kim.a@gmail.com>
2018-08-20 22:28:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,10 @@
 
 def speak_to_grandma(say)
   if say == say.upcase
-    return "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   elsif say == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   else
-    return "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
+  end
 end
